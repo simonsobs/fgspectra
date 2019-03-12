c3aa4d4e4f7779cf2fb552c5d18411e34a7a90a3
Maximilian H Abitbol <mabitbol15@gmail.com>
2019-03-12 15:28:43 -0400

    initializing these things.

---
+++ spectra.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy import constants
+
+# With inspiration from BeFoRe and FGBuster by David Alonso and Ben Thorne and 
+# Davide Poletti and Josquin Errard
+
+class PowerSpectrum:
+    def __init__(self, sed_name=''):
+        self.sed = sed_name or something
+        return
+    
+    def __call__(self, nu, args):
+        return self.sed(nu, args)
+        
+
+
+def things(nu, args, kwargs):
+    return nu  
+
+# CMB
+# blackbody, derivative BB
+
+# Galactic foregrounds
+# power law (synch), modified blackbody (dust)
+# extensions of these? 
+# before and fgbuster
+
+# Extragalactic
+# mystery
+# tile-c 
+
+# thermodynamic CMB units - defualt
+# other units: Jy/sr or Rayleigh-Jeans, full antenna temp conversion? 
