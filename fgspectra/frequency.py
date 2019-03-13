355801f43c5af53624f6a830fa2c2db569fe0b7b
Zack Li <xzackli@gmail.com>
2019-03-12 22:14:45 -0400

    more documentation, code guidelines, boilerplate

--- fgspectra/frequency.py
+++ fgspectra/frequency.py
@@ -1,33 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Frequency-dependent foreground components.
+
+This module implements the frequency-dependent component of common foreground
+contaminants. This consist only of factorizable components -- i.e. the components
+in this module describe f for which the component X has spectrum
+
+.. math:: C_{\ell}^{X} = f(\nu) C^X_{0,\ell}
+"""
 import numpy as np
 from scipy import constants
 
-# With inspiration from BeFoRe and FGBuster by David Alonso and Ben Thorne and 
+# With inspiration from BeFoRe and FGBuster by David Alonso and Ben Thorne and
 # Davide Poletti and Josquin Errard
 
-class FrequencyScaling:
+class FrequencySpectrum:
     def __init__(self, sed_name=''):
         self.sed = sed_name or something
         return
-    
+
     def __call__(self, nu, args):
         return self.sed(nu, args)
-        
+
 
 
 def things(nu, args, kwargs):
-    return nu  
+    return nu
 
 # CMB
 # blackbody, derivative BB
 
 # Galactic foregrounds
 # power law (synch), modified blackbody (dust)
-# extensions of these? 
+# extensions of these?
 # before and fgbuster
 
 # Extragalactic
 # mystery
-# tile-c 
+# tile-c
 
 # thermodynamic CMB units - defualt
-# other units: Jy/sr or Rayleigh-Jeans, full antenna temp conversion? 
+# other units: Jy/sr or Rayleigh-Jeans, full antenna temp conversion?
--- fgspectra/power.py
+++ fgspectra/power.py
@@ -1,25 +1,25 @@
 import numpy as np
 from scipy import constants
 
-# With inspiration from BeFoRe and FGBuster by David Alonso and Ben Thorne and 
+# With inspiration from BeFoRe and FGBuster by David Alonso and Ben Thorne and
 # Davide Poletti and Josquin Errard
 
 class PowerSpectrum:
     def __init__(self, ps_name=''):
         self.ps = sed_name or something
         return
-    
+
     def __call__(self, ell, args):
         return self.ps(ell, args)
-        
+
 
 
 def things(ell, args, kwargs):
     return ell
 
 
-# Power law in ell 
+# Power law in ell
 
 # Extragalactic FGs: from tile-c?? or Erminia / Jo
 
-# CMB template cls? 
+# CMB template cls?
