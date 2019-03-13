6479eac004626d77c7b108e2a81bee5a9e52951a
Zack Li <xzackli@gmail.com>
2019-03-12 21:55:56 -0400

    added usual python structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""`fgspectra` is developed at GitHub <https://github.com/simonsobs/fgspectra>."""
+
+from distutils.core import setup
+
+setup(name='fgspectra',
+      version='0.1',
+      description='Foreground SED and power spectrum library',
+      author='Simons Observatory fgspectra crew',
+      author_email='',
+      packages=['fgspectra']
+      )
