6f1388b5223333359a29542411024ab34905ae1e
Zack Li <xzackli@gmail.com>
2019-03-14 13:24:46 -0400

    pip should install data files now, changed to setuptools

--- fgspectra/power.py
+++ fgspectra/power.py
@@ -1,22 +1,44 @@
+# -*- coding: utf-8 -*-
+r"""
+Frequency-dependent foreground components.
+
+This module implements the frequency-dependent component of common foreground
+contaminants. This consist only of factorizable components -- i.e. the
+components in this module describe :math:`f(\nu)` for which the component X has
+spectrum
+
+.. math:: C_{\ell}^{X} = f(\nu) C^X_{0,\ell}
+
+This module draws inspiration from FGBuster (Davide Poletti and Josquin Errard)
+and BeFoRe (David Alonso and Ben Thorne).
+"""
 import numpy as np
 from scipy import constants
 
-# With inspiration from BeFoRe and FGBuster by David Alonso and Ben Thorne and
-# Davide Poletti and Josquin Errard
 
 class PowerSpectrum:
-    def __init__(self, ps_name=''):
-        self.ps = sed_name
+    """Base class for frequency dependent components."""
+
+    def __init__(self, name=''):
+        """Initialize the component."""
+        self.spec_name = name
         return
 
-    def __call__(self, ell, *args):
-        return self.ps(ell, args)
+    def __call__(self, ell, **kwargs):
+        """Make component objects callable."""
+        return self.powspec(ell, **kwargs)
 
 
+class tSZ1(PowerSpectrum):
+    """PowerSpectrum for Thermal Sunyaev-Zel'dovich (Dunkley et al. 2013)."""
 
-def things(nu, *args, **kwargs):
-    """Instantiate the right object and calls it, for convenience."""
-    return nu
+    def __init__(self):
+        """Intialize object with parameters."""
+        return
+
+    def powspec(ell):
+        """Compute the power spectrum with the given ell and parameters."""
+        return ell
 
 
 # Power law in ell
