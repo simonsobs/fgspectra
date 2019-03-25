cb566144c57963b6b80dc49b024b5bda3f9f47fd
dpole <davide.pole@gmail.com>
2019-03-25 11:49:09 +0100

    Proposal for several changes

--- fgspectra/power.py
+++ fgspectra/power.py
@@ -1,51 +1,45 @@
-# -*- coding: utf-8 -*-
 r"""
-Frequency-dependent foreground components.
+Power spectrum
 
 This module implements the ell-dependent component of common foreground
-contaminants. This consist only of factorizable components -- i.e. the
-components in this module describe :math:`f(\nu)` for which the component X has
-spectrum
-
-.. math:: C_{\ell}^{X} = f(\nu) C^X_{0,\ell}
+contaminants.
 
 This module draws inspiration from FGBuster (Davide Poletti and Josquin Errard)
 and BeFoRe (David Alonso and Ben Thorne).
 """
-import numpy as np
-from scipy import constants
-import pkg_resources
+
 import os
+import pkg_resources
+from abc import ABC, abstractmethod
+import numpy as np
 
 
-class PowerSpectrum:
+class PowerSpectrum(ABC):
     """Base class for frequency dependent components."""
 
-    def __init__(self):
-        """Initialize the component."""
-        pass
-
-    def __call__(self, ell, **kwargs):
+    @abstractmethod
+    def __call__(self, ell, *args):
         """Make component objects callable."""
-        return self.powspec(ell, **kwargs)
+        pass
 
 
 class PowerSpectrumFromFile(PowerSpectrum):
     """Generic PowerSpectrum loaded from file."""
 
     def __init__(self, filename):
-        """Intialize object with parameters.
+        """
 
         The file format should be two columns, ell and the spectrum.
         """
         data_path = pkg_resources.resource_filename('fgspectra', 'data/')
         file_path = os.path.join(data_path, filename)
-        self.data_ell, self.data_spec = np.genfromtxt(file_path, unpack=True)
-        return
+        ell, spec = np.genfromtxt(file_path, unpack=True)
+        self._cl = np.zeros(ell.max() + 1)
+        self._cl[ell] = spec
 
-    def powspec(self, ell):
+    def __call__(self, ell):
         """Compute the power spectrum with the given ell and parameters."""
-        return np.interp(ell, self.data_ell, self.data_spec)
+        return self._cl[ell]
 
 
 class tSZ_150_bat(PowerSpectrumFromFile):
@@ -54,7 +48,6 @@ class tSZ_150_bat(PowerSpectrumFromFile):
     def __init__(self):
         """Intialize object with parameters."""
         super().__init__("cl_tsz_150_bat.dat")
-        return
 
 
 class kSZ_bat(PowerSpectrumFromFile):
@@ -63,7 +56,6 @@ class kSZ_bat(PowerSpectrumFromFile):
     def __init__(self):
         """Intialize object with parameters."""
         super().__init__("cl_ksz_bat.dat")
-        return
 
 
 class sz_x_cib_template(PowerSpectrumFromFile):
@@ -72,33 +64,33 @@ class sz_x_cib_template(PowerSpectrumFromFile):
     def __init__(self):
         """Intialize object with parameters."""
         super().__init__("sz_x_cib_template.dat")
-        return
 
 
 class PowerLaw(PowerSpectrum):
-    r"""
-    PowerSpectrum for a power law.
-    .. math:: f(\nu) = (nu / nu_0)^{\beta}
+    r""" Power law
 
-    Parameters
-    ----------
-    ell: float or array
-        Frequency in Hz.
-    beta: float
-        Spectral index.
-    ell_0: float
-        Reference ell
-
-    Methods
-    -------
-    __call__(self, ell, beta, ell_0)
-        return the ell scaling given by a power law.
+    .. math:: C_\ell = (\ell / \ell_0)^\alpha
     """
+    def __call__(self, ell, alpha, ell_0):
+        """
 
-    def powspec(self, ell, beta, ell_0):
-        return (ell/ell_0)**beta
+        Parameters
+        ----------
+        ell: float or array
+            Multipole
+        alpha: float or array
+            Spectral index.
+        ell_0: float
+            Reference ell
 
-
+        Returns
+        -------
+        cl: ndarray
+            The last dimension is ell.
+            The leading dimensions are the hypothetic dimensions of `alpha`
+        """
+        alpha = np.array(alpha)[..., np.newaxis]
+        return (ell / ell_0)**alpha
 
 
 # Power law in ell
