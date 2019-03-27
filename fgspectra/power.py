07134f6a6674f46bd405b674945fb03a0c6d9985
dpole <davide.pole@gmail.com>
2019-03-27 16:36:35 +0100

    PowerSpectrumFromFile with arbitrary shape

--- fgspectra/power.py
+++ fgspectra/power.py
@@ -13,6 +13,15 @@ import pkg_resources
 from abc import ABC, abstractmethod
 import numpy as np
 
+def _get_power_file(model):
+    """ File path for the named model
+    """
+    data_path = pkg_resources.resource_filename('fgspectra', 'data/')
+    filename = os.path.join(data_path, 'cl_%s.dat'%model)
+    if os.path.exists(filename):
+        return filename
+    raise ValueError('No template for model '+model)
+
 
 class PowerSpectrum(ABC):
     """Base class for frequency dependent components."""
@@ -24,22 +33,61 @@ class PowerSpectrum(ABC):
 
 
 class PowerSpectrumFromFile(PowerSpectrum):
-    """Generic PowerSpectrum loaded from file."""
+    """Power spectrum loaded from file(s)
 
-    def __init__(self, filename):
+    Parameters
+    ----------
+    filenames: array_like of strings
+        File(s) to load. It can be a string or any (nested) sequence of strings
+
+    Examples
+    --------
+
+    >>> ell = range(5)
+
+    Power spectrum of a single file
+
+    >>> my_file = 'cl.dat'
+    >>> ps = PowerSpectrumFromFile(my_file)
+    >>> ps(ell).shape
+    (5)
+    >>> ps = PowerSpectrumFromFile([my_file])  # List
+    >>> ps(ell).shape
+    (1, 5)
+    
+    Two correlated components
+
+    >>> my_files = [['cl_comp1.dat', 'cl_comp1xcomp2.dat'],
+    ...             ['cl_comp1xcomp2.dat', 'cl_comp2.dat']]
+    >>> ps = PowerSpectrumFromFile(my_files)
+    >>> ps(ell).shape
+    (2, 2, 5)
+
+    """
+
+    def __init__(self, filenames):
         """
 
         The file format should be two columns, ell and the spectrum.
         """
-        data_path = pkg_resources.resource_filename('fgspectra', 'data/')
-        file_path = os.path.join(data_path, filename)
-        ell, spec = np.genfromtxt(file_path, unpack=True)
-        self._cl = np.zeros(ell.max() + 1)
-        self._cl[ell] = spec
+        filenames = np.array(filenames)
+        self._cl = np.empty(filenames.shape+(0,))
+
+        for i, filename in np.ndenumerate(filenames):
+            ell, spec = np.genfromtxt(filename, unpack=True)
+
+            # Make sure that the new spectrum fits self._cl
+            n_missing_ells = ell.max() + 1 - self._cl.shape[-1]
+            if n_missing_ells > 0:
+                self._cl = np.pad(self._cl, ((0,0), (0, n_missing_ells)),
+                                  mode='constant', constant_values=0)
+
+            self._cl[i+(ell,)] = spec
+
 
     def __call__(self, ell):
         """Compute the power spectrum with the given ell and parameters."""
-        return self._cl[ell]
+        return self._cl[..., ell]
 
 
 class tSZ_150_bat(PowerSpectrumFromFile):
@@ -47,7 +95,7 @@ class tSZ_150_bat(PowerSpectrumFromFile):
 
     def __init__(self):
         """Intialize object with parameters."""
-        super().__init__("cl_tsz_150_bat.dat")
+        super().__init__(_get_power_file('tsz_150_bat'))
 
 
 class kSZ_bat(PowerSpectrumFromFile):
@@ -55,7 +103,7 @@ class kSZ_bat(PowerSpectrumFromFile):
 
     def __init__(self):
         """Intialize object with parameters."""
-        super().__init__("cl_ksz_bat.dat")
+        super().__init__(_get_power_file('ksz_bat'))
 
 
 class sz_x_cib_template(PowerSpectrumFromFile):
@@ -63,7 +111,7 @@ class sz_x_cib_template(PowerSpectrumFromFile):
 
     def __init__(self):
         """Intialize object with parameters."""
-        super().__init__("sz_x_cib_template.dat")
+        super().__init__(_get_power_file('sz_x_cib'))
 
 
 class PowerLaw(PowerSpectrum):
