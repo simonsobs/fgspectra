35b53f9e4485bee048c2395190359a405ab8507f
dpole <davide.pole@gmail.com>
2019-06-06 14:48:08 +0200

    Model class, defaults change and readable print

--- fgspectra/cross.py
+++ fgspectra/cross.py
@@ -8,18 +8,10 @@ from abc import ABC, abstractmethod
 import numpy as np
 from . import frequency as fgf
 from . import power as fgp
+from .model import Model
 
 
-class CrossSpectrum(ABC):
-    """Base class for cross-spectra."""
-
-    @abstractmethod
-    def __call__(self, *args):
-        """Make it callable."""
-        pass
-
-
-class FactorizedCrossSpectrum(CrossSpectrum):
+class FactorizedCrossSpectrum(Model):
     r"""Factorized cross-spectrum
 
     Cross-spectrum of **one** component for which the scaling in frequency
@@ -42,17 +34,36 @@ class FactorizedCrossSpectrum(CrossSpectrum):
     broadcast-compatible.
     """
 
-    def __init__(self, sed, cl):
+    def __init__(self, sed, cl, **kwargs):
         self._sed = sed
         self._cl = cl
-        
-    def __str__(self):
-        """Inspect list of SED and Cl signatures."""
-        import inspect
-        return (f"SED arguments: {inspect.signature(self._sed)}\n"
-                f"Cl arguments: {inspect.signature(self._cl)}")
+        self.set_defaults(**kwargs)
 
-    def __call__(self, sed_kwargs=None, cl_kwargs=None):
+    def set_defaults(self, **kwargs):
+        if 'sed_kwargs' in kwargs:
+            self._sed.set_defaults(**kwargs['sed_kwargs'])
+        if 'cl_kwargs' in kwargs:
+            self._cl.set_defaults(**kwargs['cl_kwargs'])
+
+    @property
+    def defaults(self):
+        return {
+            'sed_kwargs': self._sed.defaults,
+            'cl_kwargs': self._cl.defaults
+        }
+
+    def _get_repr(self):
+        sed_repr = self._sed._get_repr()
+        key = list(sed_repr.keys())[0]
+        sed_repr[key + ' (SED)'] = sed_repr.pop(key)
+
+        cl_repr = self._cl._get_repr()
+        key = list(cl_repr.keys())[0]
+        cl_repr[key + ' (Cl)'] = cl_repr.pop(key)
+
+        return {type(self).__name__: [sed_repr, cl_repr]}
+
+    def eval(self, sed_kwargs={}, cl_kwargs={}):
         """Compute the model at frequency and ell combinations.
 
         Parameters
@@ -71,8 +82,8 @@ class FactorizedCrossSpectrum(CrossSpectrum):
         return f_nu[..., np.newaxis] * f_nu * self._cl(**cl_kwargs)
 
 
-class CorrelatedFactorizedCrossSpectrum(CrossSpectrum):
-    r"""Factorized cross-spectrum
+class CorrelatedFactorizedCrossSpectrum(FactorizedCrossSpectrum):
+    r"""Factorized cross-spectrum of correlated components
 
     Cross-spectrum of multiple correlated components for which the scaling in
     frequency and in multipoles are factorizable.
@@ -99,17 +110,12 @@ class CorrelatedFactorizedCrossSpectrum(CrossSpectrum):
     broadcast-compatible.
     """
 
-    def __init__(self, sed, cl):
+    def __init__(self, sed, cl, **kwargs):
         self._sed = sed
         self._cl = cl
+        self.set_defaults(**kwargs)
 
-    def __str__(self):
-        """Inspect list of SED and Cl signatures."""
-        import inspect
-        return (f"SED arguments: {[inspect.signature(s) for s in self._sed._seds]}\n"
-                f"Cl arguments: {[inspect.signature(c) for c in self._cl._power_spectra]}")
-
-    def __call__(self, sed_kwargs=None, cl_kwargs=None):
+    def eval(self, sed_kwargs={}, cl_kwargs={}):
         """Compute the model at frequency and ell combinations.
 
         Parameters
@@ -137,8 +143,9 @@ class PowerLaw(FactorizedCrossSpectrum):
     :class:`fgspectra.power.PowerLaw` for the ell-dependence.
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__(fgf.PowerLaw(), fgp.PowerLaw())
+        self.set_defaults(**kwargs)
 
 
 class CorrelatedPowerLaw(CorrelatedFactorizedCrossSpectrum):
@@ -148,8 +155,9 @@ class CorrelatedPowerLaw(CorrelatedFactorizedCrossSpectrum):
     :class:`fgspectra.power.PowerLaw` for the ell-dependence.
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__(fgf.PowerLaw(), fgp.CorrelatedPowerLaws())
+        self.set_defaults(**kwargs)
 
 
 class CorrelatedDustSynchrotron(CorrelatedFactorizedCrossSpectrum):
@@ -158,15 +166,17 @@ class CorrelatedDustSynchrotron(CorrelatedFactorizedCrossSpectrum):
     Correlated power law and modified black body, both with power law amplitude
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         super().__init__(
             fgf.Join(fgf.ModifiedBlackBody(), fgf.PowerLaw()),
             fgp.CorrelatedPowerLaws()
         )
+        self.set_defaults(**kwargs)
 
 
 class SZxCIB(CorrelatedFactorizedCrossSpectrum):
     
-    def __init__(self):
+    def __init__(self, **kwargs):
         sed = fgf.Join(fgf.ThermalSZ(), fgf.CIB())
         super().__init__(sed, fgp.SZxCIB())
+        self.set_defaults(**kwargs)
--- fgspectra/frequency.py
+++ fgspectra/frequency.py
@@ -9,59 +9,30 @@ This package draws inspiration from FGBuster (Davide Poletti and Josquin Errard)
 and BeFoRe (David Alonso and Ben Thorne).
 """
 
-from abc import ABC, abstractmethod
 import numpy as np
 from scipy import constants
+from .model import Model
+from functools import wraps
 
 
 T_CMB = 2.72548
 H_OVER_KT_CMB = constants.h * 1e9 / constants.k / T_CMB
 
-def _to_cmb(nu):
+def _rj2cmb(nu):
     x = H_OVER_KT_CMB * nu
     return (np.expm1(x) / x)**2 / np.exp(x)
 
 
-def convert_to_k_cmb_unless_rj_is_true(f):
-    """ Convert to CMB units by defaults
-    
-    Unless RJ=TRUE in the arguments, convert the output in CMB units,
-    making use of the ``nu`` keyword argument. 
-    If `nu_0` is among the arguments, normalize the conversion factor at that
-    frequency to one.
-    """
-    def f_cmb_by_default(self, **kwargs):
-        # We'll add RJ option in the future
-        #if 'RJ' in kwargs:
-            #use_rj = kwargs['RJ']
-            #del kwargs['RJ']
-            #if use_rj:
-                #return f(self, **kwargs)
-        factor = _to_cmb(kwargs['nu'])
-        if 'nu_0' in kwargs:
-            factor /= _to_cmb(kwargs['nu_0'])
-        return f(self, **kwargs) * factor
-    return f_cmb_by_default
-
-
-
-class SED(ABC):
-    """Base class for frequency dependent components."""
-
-    @abstractmethod
-    def __call__(self, **kwargs):
-        # Return the evaluation of the SED
-        pass
-
-
-class PowerLaw(SED):
+class PowerLaw(Model):
     r""" Power Law
 
     .. math:: f(\nu) = (\nu / nu_0)^{\beta}
     """
+    def __init__(self, **kwargs):
+        """ You can set defaults at construction time """
+        self.set_defaults(**kwargs)
 
-    @convert_to_k_cmb_unless_rj_is_true
-    def __call__(self, nu=None, beta=None, nu_0=None):
+    def eval(self, nu=None, beta=None, nu_0=None):
         """ Evaluation of the SED
 
         Parameters
@@ -100,7 +71,7 @@ class PowerLaw(SED):
         """
         beta = np.array(beta)[..., np.newaxis]
         nu_0 = np.array(nu_0)[..., np.newaxis]
-        return (nu / nu_0)**beta
+        return (nu / nu_0)**beta * _rj2cmb(nu)
 
 
 class Synchrotron(PowerLaw):
@@ -109,15 +80,14 @@ class Synchrotron(PowerLaw):
     pass
 
 
-class ModifiedBlackBody(SED):
+class ModifiedBlackBody(Model):
     """ Modified black body in K_RJ
 
     .. math:: f(\nu) = (nu / nu_0)^{\beta + 1} / (e^x - 1)
 
     where :math:`x = h \nu / k_B T_d`
     """
-    @convert_to_k_cmb_unless_rj_is_true
-    def __call__(self, nu=None, nu_0=None, temp=None, beta=None):
+    def eval(self, nu=None, nu_0=None, temp=None, beta=None):
         """ Evaluation of the SED
 
         Parameters
@@ -142,7 +112,8 @@ class ModifiedBlackBody(SED):
         temp = np.array(temp)[..., np.newaxis]
         x = 1e+9 * constants.h * nu / (constants.k * temp)
         x_0 = 1e+9 * constants.h * nu_0 / (constants.k * temp)
-        return (nu / nu_0)**(beta + 1.0) * np.expm1(x_0) / np.expm1(x)
+        res = (nu / nu_0)**(beta + 1.0) * np.expm1(x_0) / np.expm1(x)
+        return res * (_rj2cmb(nu) / _rj2cmb(nu_0))
 
 
 class CIB(ModifiedBlackBody):
@@ -151,7 +122,7 @@ class CIB(ModifiedBlackBody):
     pass
 
 
-class ThermalSZ(SED):
+class ThermalSZ(Model):
     r""" Thermal Sunyaev-Zel'dovich in K_CMB
 
     This class implements the
@@ -166,7 +137,7 @@ class ThermalSZ(SED):
         x = constants.h * (nu * 1e9) / (constants.k * T_CMB)
         return (x / np.tanh(x / 2.0) - 4.0)
 
-    def __call__(self, nu, nu_0):
+    def eval(self, nu, nu_0):
         """Compute the SED with the given frequency and parameters.
 
         nu : float
@@ -176,29 +147,53 @@ class ThermalSZ(SED):
         return ThermalSZ.f(nu) / ThermalSZ.f(nu_0)
 
 
-class UnitSED(SED):
+class ConstantSED(Model):
     """Frequency-independent component."""
 
-    def __call__(self, nu, *args):
-        return np.ones_like(np.array(nu))
+    def eval(self, nu=None, amp=1.):
+        amp = np.array(amp)
+        return amp * np.ones_like(np.array(nu)) * _rj2cmb(nu)
 
 
-class Join(SED):
+class Join(Model):
+    """ Join several SED models together
+    """
 
-    def __init__(self, *seds):
+    def __init__(self, *seds, **kwargs):
+        """ Join several SED models together
+
+        Parameters
+        ----------
+        *sed:
+            Sequence of SED models to be joined together
+        """
         self._seds = seds
 
-    def __call__(self, kwargs_seq=None):
+    def set_defaults(self, **kwargs):
+        if 'kwseq' in kwargs:
+            for sed, sed_kwargs in zip(self._seds, kwargs['kwseq']):
+                sed.set_defaults(**sed_kwargs)
+
+    def _get_repr(self):
+        return {type(self).__name__: [sed._get_repr() for sed in self._seds]}
+
+    @property
+    def defaults(self):
+        return {'kwseq': [sed.defaults for sed in self._seds]}
+
+    def eval(self, kwseq=None):
         """Compute the SED with the given frequency and parameters.
 
-        *kwargss
-            The length of ``kwargss`` has to be equal to the number of SEDs
-            joined. ``kwargss[i]`` is a dictionary containing the keyword
+        *kwseq
+            The length of ``kwseq`` has to be equal to the number of SEDs
+            joined. ``kwseq[i]`` is a dictionary containing the keyword
             arguments of the ``i``-th SED.
         """
-        seds = [sed(**kwargs) for sed, kwargs in zip(self._seds, kwargs_seq)]
+        if kwseq:
+            seds = [sed(**kwargs) for sed, kwargs in zip(self._seds, kwseq)]
+        else:
+            seds = [sed() for sed in self._seds]
         res = np.empty((len(seds),) + np.broadcast(*seds).shape)
         for i in range(len(seds)):
             res[i] = seds[i]
         return res
-
