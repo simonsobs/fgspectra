Fix ell0 in PS from file and broadcast amplitude

fgspectra/power.py
@@ -81,7 +81,8 @@ class PowerSpectrumFromFile(Model):
 
     def eval(self, ell=None, ell_0=None, amp=1.0):
         """Compute the power spectrum with the given ell and parameters."""
-        return amp * self._cl[..., ell] / self._cl[..., ell_0, np.newaxis]
+        amp = np.array(amp)[..., np.newaxis]
+        return amp / self._cl[..., ell_0] * self._cl[..., ell]
 
 
 class tSZ_150_bat(PowerSpectrumFromFile):