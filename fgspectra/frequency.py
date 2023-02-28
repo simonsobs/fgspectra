d17cd61c27b030dbc917e96774391aecd083e6b7
xzackli <xzackli@gmail.com>
2023-02-28 12:47:58 -0500

    merge changes from main

--- fgspectra/frequency.py
+++ fgspectra/frequency.py
@@ -19,8 +19,9 @@ from .model import Model
 T_CMB = 2.72548
 H_OVER_KT_CMB = constants.h * 1e9 / constants.k / T_CMB
 
+
 def _bandpass_integration():
-    ''' Bandpass integrated version of the caller
+    """Bandpass integrated version of the caller
 
     The caller should have
 
@@ -40,14 +41,14 @@ def _bandpass_integration():
     * stacks the output of the iteration (the frequency dimension is the last)
       and returns it
 
-    '''
+    """
     # This piece of code is fairly complicated, we did because:
     # 1) We want to call eval on each element of the nu list (i.e. we iterate
     #    over the bandpasses) but we don't want to define a new eval_bandpass
     #    function for every class
     # 2) We don't want to use a decorator because it breaks the signature
     #    handling of eval and the modification of its defaults.
-    #    _bandpass_integration does from the insides of eval the same thing that 
+    #    _bandpass_integration does from the insides of eval the same thing that
     #    a decorator would do from the outside. This is achieved through the
     #    following pretty ugly kludge
     # Simpler code that achieve the same result is welcome
@@ -59,25 +60,25 @@ def _bandpass_integration():
     # It assumes that _bandpass_integration was called inside
     # f(self, **kw) -- f is typically the eval method.
     frame = inspect.currentframe().f_back
-    kw = frame.f_locals  
-    self = kw['self']
-    del kw['self']  # self was in the locals but is not a keyword argument
+    kw = frame.f_locals
+    self = kw["self"]
+    del kw["self"]  # self was in the locals but is not a keyword argument
 
     # We create a copy of eval itself, we'll call it for each bandpass
     f = types.FunctionType(frame.f_code, frame.f_globals)
     # Store the nu-transmittance list because the nu keyword argumnt has to be
     # modified with the frequencies of each bandpass
-    nus_transmittances = kw['nu']
+    nus_transmittances = kw["nu"]
 
     # Get the shape of the output from the result of the first bandpass
-    kw['nu'] = nus_transmittances[0][0]
-    res = np.trapz(f(self, **kw) * nus_transmittances[0][1], kw['nu'])
+    kw["nu"] = nus_transmittances[0][0]
+    res = np.trapz(f(self, **kw) * nus_transmittances[0][1], kw["nu"])
     # Append the frequency dimension and put res in its first entry
-    res = res[..., np.newaxis] * np.array([1.]+[0.]*(len(nus_transmittances)-1))
-    
+    res = res[..., np.newaxis] * np.array([1.0] + [0.0] * (len(nus_transmittances) - 1))
+
     # Fill the remaining entries by iterating over the rest of the bandpasses
     for i_band, (nu, transmittance) in enumerate(nus_transmittances[1:], 1):
-        kw['nu'] = nu
+        kw["nu"] = nu
         res[..., i_band] = np.trapz(f(self, **kw) * transmittance, nu)
 
     return res
@@ -85,16 +86,17 @@ def _bandpass_integration():
 
 def _rj2cmb(nu):
     x = H_OVER_KT_CMB * nu
-    return (np.expm1(x) / x)**2 / np.exp(x)
+    return (np.expm1(x) / x) ** 2 / np.exp(x)
 
 
 class PowerLaw(Model):
-    r""" Power Law
+    r"""Power Law
 
     .. math:: f(\nu) = (\nu / \nu_0)^{\beta}
     """
+
     def eval(self, nu=None, beta=None, nu_0=None):
-        """ Evaluation of the SED
+        """Evaluation of the SED
 
         Parameters
         ----------
@@ -135,24 +137,25 @@ class PowerLaw(Model):
 
         beta = np.array(beta)[..., np.newaxis]
         nu_0 = np.array(nu_0)[..., np.newaxis]
-        return (nu / nu_0)**beta * (_rj2cmb(nu) / _rj2cmb(nu_0))
+        return (nu / nu_0) ** beta * (_rj2cmb(nu) / _rj2cmb(nu_0))
 
 
 class Synchrotron(PowerLaw):
-    """ Alias of :class:`PowerLaw`
-    """
+    """Alias of :class:`PowerLaw`"""
+
     pass
 
 
 class ModifiedBlackBody(Model):
-    r""" Modified black body in K_RJ
+    r"""Modified black body in K_RJ
 
     .. math:: f(\nu) = (\nu / \nu_0)^{\beta + 1} / (e^x - 1)
 
     where :math:`x = h \nu / k_B T_d`
     """
+
     def eval(self, nu=None, nu_0=None, temp=None, beta=None):
-        """ Evaluation of the SED
+        """Evaluation of the SED
 
         Parameters
         ----------
@@ -174,23 +177,23 @@ class ModifiedBlackBody(Model):
         """
         if isinstance(nu, list):
             return _bandpass_integration()
-        
+
         beta = np.array(beta)[..., np.newaxis]
         temp = np.array(temp)[..., np.newaxis]
-        x = 1e+9 * constants.h * nu / (constants.k * temp)
-        x_0 = 1e+9 * constants.h * nu_0 / (constants.k * temp)
-        res = (nu / nu_0)**(beta + 1.0) * np.expm1(x_0) / np.expm1(x)
+        x = 1e9 * constants.h * nu / (constants.k * temp)
+        x_0 = 1e9 * constants.h * nu_0 / (constants.k * temp)
+        res = (nu / nu_0) ** (beta + 1.0) * np.expm1(x_0) / np.expm1(x)
         return res * (_rj2cmb(nu) / _rj2cmb(nu_0))
 
 
 class CIB(ModifiedBlackBody):
-    """ Alias of :class:`ModifiedBlackBOdy`
-    """
+    """Alias of :class:`ModifiedBlackBOdy`"""
+
     pass
 
 
 class ThermalSZ(Model):
-    r""" Thermal Sunyaev-Zel'dovich in K_CMB
+    r"""Thermal Sunyaev-Zel'dovich in K_CMB
 
     This class implements the
 
@@ -202,7 +205,7 @@ class ThermalSZ(Model):
     @staticmethod
     def f(nu):
         x = constants.h * (nu * 1e9) / (constants.k * T_CMB)
-        return (x / np.tanh(x / 2.0) - 4.0)
+        return x / np.tanh(x / 2.0) - 4.0
 
     def eval(self, nu=None, nu_0=None):
         """Compute the SED with the given frequency and parameters.
@@ -218,13 +221,14 @@ class ThermalSZ(Model):
 
 
 class FreeFree(Model):
-    r""" Free-free
+    r"""Free-free
 
     .. math:: f(\nu) = EM * ( 1 + log( 1 + (\nu_{ff} / \nu)^{3/\pi} ) )
     .. math:: \nu_{ff} = 255.33e9 * (Te / 1000)^{3/2}
     """
+
     def eval(self, nu=None, EM=None, Te=None):
-        """ Evaluation of the SED
+        """Evaluation of the SED
 
         Parameters
         ----------
@@ -257,19 +261,20 @@ class FreeFree(Model):
         if isinstance(nu, list):
             return _bandpass_integration()
 
-        EM  = np.array(EM)[..., np.newaxis]
-        Te  = np.array(Te)[..., np.newaxis]
-        Teff = (Te / 1.e3)**(1.5)
+        EM = np.array(EM)[..., np.newaxis]
+        Te = np.array(Te)[..., np.newaxis]
+        Teff = (Te / 1.0e3) ** (1.5)
         nuff = 255.33e9 * Teff
-        gff = 1. + np.log(1. + (nuff / nu)**(np.sqrt(3) / np.pi))
+        gff = 1.0 + np.log(1.0 + (nuff / nu) ** (np.sqrt(3) / np.pi))
         print("warning: I need to check the units on this")
         return EM * gff
+
 
 class ConstantSED(Model):
     """Frequency-independent component."""
 
-    def eval(self, nu=None, amp=1.):
-        """ Evaluation of the SED
+    def eval(self, nu=None, amp=1.0):
+        """Evaluation of the SED
 
         Parameters
         ----------
@@ -293,11 +298,10 @@ class ConstantSED(Model):
 
 
 class Join(Model):
-    """ Join several SED models together
-    """
+    """Join several SED models together"""
 
     def __init__(self, *seds, **kwargs):
-        """ Join several SED models together
+        """Join several SED models together
 
         Parameters
         ----------
@@ -308,8 +312,8 @@ class Join(Model):
         self.set_defaults(**kwargs)
 
     def set_defaults(self, **kwargs):
-        if 'kwseq' in kwargs:
-            for sed, sed_kwargs in zip(self._seds, kwargs['kwseq']):
+        if "kwseq" in kwargs:
+            for sed, sed_kwargs in zip(self._seds, kwargs["kwseq"]):
                 sed.set_defaults(**sed_kwargs)
 
     def _get_repr(self):
@@ -317,7 +321,7 @@ class Join(Model):
 
     @property
     def defaults(self):
-        return {'kwseq': [sed.defaults for sed in self._seds]}
+        return {"kwseq": [sed.defaults for sed in self._seds]}
 
     def eval(self, kwseq=None):
         """Compute the SED with the given frequency and parameters.
--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@
 from setuptools import setup
 
 setup(
-    name='fgspectra',
-    version='0.1',
-    description='Foreground SED and power spectrum library',
-    author='Simons Observatory fgspectra crew',
-    author_email='',
-    packages=['fgspectra'],
-    python_requires='>3.5',
-    install_requires = [
-        'scipy',
-        'pyyaml',
+    name="fgspectra",
+    version="0.1",
+    description="Foreground SED and power spectrum library",
+    author="Simons Observatory fgspectra crew",
+    author_email="",
+    packages=["fgspectra"],
+    python_requires=">3.5",
+    install_requires=[
+        "scipy",
+        "pyyaml",
     ],
-    include_package_data=True
+    include_package_data=True,
 )
