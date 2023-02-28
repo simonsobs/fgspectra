fb647d53f888468709becbc1c5bcc39ad889dcdb
xzackli <xzackli@gmail.com>
2023-02-27 23:19:44 -0500

    black formatting for tests and config

--- tests/test_frequency.py
+++ tests/test_frequency.py
@@ -5,11 +5,9 @@ from numpy.testing import assert_allclose as aac
 
 
 def test_bandpass_integration():
-    frequency = np.array([[10., 20., 40.],
-                          [60., 90., 100.]])
+    frequency = np.array([[10.0, 20.0, 40.0], [60.0, 90.0, 100.0]])
 
-    transmittance = np.array([[1., 2., 4.],
-                              [1., 3., 1.]])
+    transmittance = np.array([[1.0, 2.0, 4.0], [1.0, 3.0, 1.0]])
 
     class Mock(Model):
         def eval(self, nu=None, par=None):
@@ -21,11 +19,11 @@ def test_bandpass_integration():
     nus = list(zip(frequency, transmittance))
 
     ress = mock(nus, 1)
-    refs = [75., 80.]
+    refs = [75.0, 80.0]
     for res, ref in zip(refs, ress):
         aac(res, ref)
 
-    ress = mock(nus, np.array([1., 2.]))
-    refs = np.array([[75., 80.], [150., 160.]])
+    ress = mock(nus, np.array([1.0, 2.0]))
+    refs = np.array([[75.0, 80.0], [150.0, 160.0]])
     for res, ref in zip(refs, ress):
         aac(res, ref)
