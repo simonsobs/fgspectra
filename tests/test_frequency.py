56ae5caaedbcac5a89a5e7a93ee6f97c979452cc
sgiardie <grdsrn1@unife.it>
2025-09-17 16:41:59 +0100

    adding case with transmission with shape [freqs, ells] in test_frequency

--- tests/test_frequency.py
+++ tests/test_frequency.py
@@ -26,3 +26,27 @@ def test_bandpass_integration():
     refs = np.array([[75.0, 80.0], [150.0, 160.0]])
     for res, ref in zip(refs, ress):
         aac(res, ref)
+
+    # testing the case with transmittance shape [nchannels, nfreqs, ells]
+    transmittance = np.array([[[1.0, 4.0], [2.0, 5.0],[4.0, 6.0]], [[1.0, 4.0], [3.0, 5.0], [1.0, 6.0]]])
+    nus = list(zip(frequency, transmittance))
+
+    ress = mock(nus, 1)
+    refs = np.array([[ 75.,  80.],
+       [155., 190.]])
+
+    for res, ref in zip(refs, ress):
+        aac(res, ref)
+        
+    ress = mock(nus, np.array([1.0, 2.0]))
+    refs = np.array([[[ 45.,  60.],
+        [180., 240.]],
+
+       [[ 90., 180.],
+        [225., 300.]],
+
+       [[180.,  60.],
+        [270., 360.]]])
+
+    for res, ref in zip(refs, ress):
+        aac(res, ref)
