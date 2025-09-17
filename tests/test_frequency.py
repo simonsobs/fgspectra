b1a1b9394a6ad40b33099e5562e524aaacf9b3de
sgiardie <grdsrn1@unife.it>
2025-09-17 16:44:27 +0100

    reformatting

--- tests/test_frequency.py
+++ tests/test_frequency.py
@@ -28,25 +28,25 @@ def test_bandpass_integration():
         aac(res, ref)
 
     # testing the case with transmittance shape [nchannels, nfreqs, ells]
-    transmittance = np.array([[[1.0, 4.0], [2.0, 5.0],[4.0, 6.0]], [[1.0, 4.0], [3.0, 5.0], [1.0, 6.0]]])
+    transmittance = np.array(
+        [[[1.0, 4.0], [2.0, 5.0], [4.0, 6.0]], [[1.0, 4.0], [3.0, 5.0], [1.0, 6.0]]]
+    )
     nus = list(zip(frequency, transmittance))
 
     ress = mock(nus, 1)
-    refs = np.array([[ 75.,  80.],
-       [155., 190.]])
+    refs = np.array([[75.0, 80.0], [155.0, 190.0]])
 
     for res, ref in zip(refs, ress):
         aac(res, ref)
-        
+
     ress = mock(nus, np.array([1.0, 2.0]))
-    refs = np.array([[[ 45.,  60.],
-        [180., 240.]],
-
-       [[ 90., 180.],
-        [225., 300.]],
-
-       [[180.,  60.],
-        [270., 360.]]])
+    refs = np.array(
+        [
+            [[45.0, 60.0], [180.0, 240.0]],
+            [[90.0, 180.0], [225.0, 300.0]],
+            [[180.0, 60.0], [270.0, 360.0]],
+        ]
+    )
 
     for res, ref in zip(refs, ress):
         aac(res, ref)
