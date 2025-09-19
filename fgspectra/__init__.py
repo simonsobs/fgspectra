ccf76c5bb8037c5f1f635c02a69786227abdc568
Benjamin Beringue <beringueb@gmail.com>
2025-09-19 11:09:11 +0200

    Formatting

--- fgspectra/__init__.py
+++ fgspectra/__init__.py
@@ -4,6 +4,7 @@ except ImportError:
     # Fallback for development installations or if setuptools-scm fails
     try:
         from importlib.metadata import version, PackageNotFoundError
+
         try:
             __version__ = version("fgspectra")
         except PackageNotFoundError:
@@ -12,6 +13,7 @@ except ImportError:
         # Python < 3.8 fallback
         try:
             import pkg_resources
+
             __version__ = pkg_resources.get_distribution("fgspectra").version
         except Exception:
             __version__ = "unknown"
